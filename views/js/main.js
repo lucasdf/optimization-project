786ad25866b4c82524c1af7854c446d664c2394d
Lucas <lucasdiasf@gmail.com>
2014-12-15 13:04:44 -0200

    Optimize loop

--- views/js/main.js
+++ views/js/main.js
@@ -503,8 +503,10 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  var itemsLength = items.length;
+  var _phase = document.body.scrollTop / 1250
+  for (var i = 0; i < itemsLength; i++) {
+    var phase = Math.sin(_phase + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
